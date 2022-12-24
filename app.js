c3aa8a9ef69d124110ca239ab800f253f2b90bb2
Tal Cohen <cohenae@post.bgu.ac.il>
2022-12-24 16:36:54 +0200

    try

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = require("express")();
 //const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello Naama and Lior ! :)");
 });
 
 app.listen(process.env.PORT || 3000);
