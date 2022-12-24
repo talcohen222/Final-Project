a5e566464df78e153f2fa01354621308e5522c57
Tal Cohen <cohenae@post.bgu.ac.il>
2022-12-24 15:37:35 +0200

    try

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const app = require("express")();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(PORT, () => {
-    console.log(`App up at ${PORT}`);
-});
+app.listen(proces.env.PORT || 3000);
