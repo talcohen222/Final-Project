bdb69f82f637cdda31be4ff77f12929a975f9cfa
Tal Cohen <cohenae@post.bgu.ac.il>
2023-01-05 20:34:43 +0200

    try

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ app.get("/", (req, res) => {
 
 app.listen(process.env.PORT || 3000);
 
+mongoose.connect(
+  process.env.FinalPrijectDB || 'mongodb://localhost:27017/FinalPrijectDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
-const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(process.env.FinalPrijectDB, function(err, db) {
-  if (err) throw err;
-  console.log("Connected to the database!");
-  // do something with the database here
-});
