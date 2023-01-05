a40b1fdcf9d99b716006f8ce0a887e5be02b9412
Tal Cohen <cohenae@post.bgu.ac.il>
2023-01-05 20:45:38 +0200

    try

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ app.get("/", (req, res) => {
   res.send("Hello Naama and Lior !!! :)");
 });
 
-app.listen(process.env.PORT || 3000);
 
+app.listen(process.env.PORT || 3000);
+/*
 mongoose.connect(
   process.env.FinalPrijectDB || 'mongodb://localhost:27017/FinalPrijectDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-);
+);*/
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if (err) throw err;
+  console.log("Connected to the database!");
+  // do something with the database here
+});
 
 
